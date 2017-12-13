0d5aa9784e7e212ee4b8bb3a80a15e4383d68e4d
drmilde <milde@hs-fulda.de>
2017-12-13 21:39:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/milde/countdownanimation/CountdowAnimation.java
@@ -0,0 +1,50 @@
+package com.example.milde.countdownanimation;
+
+import android.content.Context;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+/**
+ * Created by milde on 13.12.17.
+ */
+
+public class CountdowAnimation implements Animation.AnimationListener {
+    private TextView tv;
+    private int count = 10;
+    private Context ctx;
+
+    private Animation anim;
+
+    public CountdowAnimation(Context ctx, TextView tv, int count) {
+        this.tv = tv;
+        this.count = count;
+        this.ctx = ctx;
+
+        anim = AnimationUtils.loadAnimation(ctx, R.anim.scale_down);
+        anim.setAnimationListener(this);
+
+    }
+
+    public void startAnimation() {
+        tv.startAnimation(anim);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        tv.setText("" + count);
+        count = count - 1;
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        anim.cancel();
+        if (count >= 0) {
+            startAnimation();
+        }
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+    }
+}
---
+++ app/src/main/java/com/example/milde/countdownanimation/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.milde.countdownanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.tvZahl);
+
+        Button btnAnimate = (Button)findViewById(R.id.btnAnimate);
+        btnAnimate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateNumber();
+            }
+        });
+
+    }
+
+    private void animateNumber() {
+        CountdowAnimation cdAnim = new CountdowAnimation(this, tv, 9);
+        cdAnim.startAnimation();
+    }
+
+}
